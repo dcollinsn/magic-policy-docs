1752033a80028637a43ad388560dff1b6375c3fc
Dan Collins <dcollinsn@gmail.com>
2017-02-15 12:51:32 -0500

    Add text conversions

---
+++ process.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+
+my @files = <*.pdf>;
+foreach my $filename (@files) {
+    if ($filename =~ /IPG_(\d{4}_\d{2}_\d{2})\.pdf/) {
+        my $date = $1;
+        my $txt = "IPG_$date.txt";
+        `pdftotext $filename`;
+        `perl -nwe 'next if /^\\d{1,2}\$/; print;' < $txt > processed_txt/$txt`;
+    }
+}
